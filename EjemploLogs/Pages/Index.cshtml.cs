f9de72445b52404b0fbe5941940007a2907eb280
Miguel Gonzalez <mig_24@hotmail.com>
2021-09-25 23:32:03 -0500

    Ejemplos Logs

---
+++ EjemploLogs/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using EjemploLogs.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjemploLogs.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> logger;
+        private readonly IEmailService emailService;
+        private readonly Serilog.ILogger seriLogger;
+
+        public IndexModel(ILogger<IndexModel> logger, IEmailService emailService, Serilog.ILogger seriLogger)
+        {
+            this.logger = logger;
+            this.emailService = emailService;
+            this.seriLogger = seriLogger;
+        }
+        public void OnGet()
+        {
+            logger.LogDebug("Este es un mensaje debug");
+            logger.LogWarning("Este es un mensaje de warning");
+            logger.LogError("Este es un mensaje de error");
+            seriLogger.Error("Este es un mensaje de error");
+            emailService.EnviarCorreo();
+        }
+    }
+}
---
+++ EjemploLogs/Startup.cs
@@ -0,0 +1,75 @@
+using EjemploLogs.Services;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Serilog;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EjemploLogs
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        [Obsolete]
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.Configure<CookiePolicyOptions>(options =>
+            {
+                // This lambda determines whether user consent for non-essential cookies is needed for a given request.
+                options.CheckConsentNeeded = context => true;
+                options.MinimumSameSitePolicy = SameSiteMode.None;
+            });
+
+
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+
+            services.AddSingleton<IEmailService, EmailService>();
+            services.AddSingleton<Serilog.ILogger>(options =>
+            {
+                var connString = Configuration["Serilog:DefaultConnection"];
+                var tableName = Configuration["Serilog:TableName"];
+                return new LoggerConfiguration().WriteTo.
+                MSSqlServer(connString, tableName,
+                restrictedToMinimumLevel: Serilog.Events.LogEventLevel.Warning,
+                autoCreateSqlTable: true).CreateLogger();
+            });  
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddFile("Log-{Date}.txt");
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            else
+            {
+                app.UseExceptionHandler("/Error");
+                app.UseHsts();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseStaticFiles();
+            app.UseCookiePolicy();
+
+            app.UseMvc();
+        }
+    }
+}
